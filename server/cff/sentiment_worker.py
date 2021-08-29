7bfdbb9d5ab99b029f3bba2fe27eea75baea958a
Jonathan Earles <jearles.rh@gmail.com>
2021-08-28 23:14:20 -0500

    more dumb shit

--- server/cff/sentiment_worker.py
+++ server/cff/sentiment_worker.py
@@ -9,9 +9,9 @@ redis_url = os.getenv("RQ_REDIS_URL", "redis://localhost:6379")
 
 conn = redis.from_url(redis_url)
 
-if __name__ == "__main__":
-    from cff.modeling import loaded_model
+from cff.modeling import loaded_model
 
+if __name__ == "__main__":
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
         worker.work()
